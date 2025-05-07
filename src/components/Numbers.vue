967dc8930953572b9652d484709936d9135b285d
Gilson Santos <dev.gpsc@gmail.com>
2025-05-07 09:00:05 -0300

    refactor code

--- src/components/Numbers.vue
+++ src/components/Numbers.vue
@@ -1,57 +1,62 @@
 <script setup lang="ts">
-const numbers = [];
-let limit = 100;
 
-function n()
-{
-	let numbers = [];
-	for(var i = 0; i < limit; i++) { numbers = [...numbers, i]; }
+import { ref, computed } from 'vue';
+//I used the reactive property since they were missing 
+const limit = ref<number>(100);
+const hoveredNumber = ref<number | null>(null);
 
-	return numbers.sort(() => Math.random() - 0.5);
-}
+/**I transformed the number relation into a "computed". 
+ * I used an array constructor to have more performance. 
+ * The previous implementation uses a for-loop which has lower performance.*/
+const numbers = computed<number[]>(() =>
+	limit.value
+		? Array.from({ length: limit.value }, (_, i) => i).sort(() => Math.random() - 0.5)
+		: []
+)
 
-function hov(number) {
-  const nums = document.querySelectorAll('.number');
+/**The "reset" and "hover" methods were removed as they were unnecessary since we can achieve the same effects through Vue's resources.
+Furthermore, manipulating the DOM directly through "querySelectorAll" and similar methods is not recommended: 
+it does not take advantage of Vue's reactivity and styling resources and can cause synchronization errors with reactive properties.*/
 
-  for(let i = 0; i < nums.length; i++)
-  {
-    const num = nums[i].textContent.trim();
-    if(number % num === 0)
-    {
-      nums[i].classList.add('active')
-      console.log('divisor', num)
-    }
-  }
-}
-
-function reset()
-{
-	const nums = document.querySelectorAll('.number');
-	nums.forEach(num => num.classList.remove('active'))
-}
 </script>
-
 <template>
 	<div>
-		<input type="number" v-model="limit" /><br /><br />
-		<div class="number"
-			:id="'number-'+number"
-			v-for="number in n()"
-			:key="number"
-			@mouseover="hov(number)"
-			@mouseout="reset"
-		>
-			{{ number }}
+		<span>
+			Hovered Number: {{ hoveredNumber }}
+		</span>
+		<input type="number" v-model="limit" />
+		<div class="container">
+
+			<div class="number" :class="{ active: hoveredNumber && hoveredNumber % number === 0 }" :id="'number-' + numbers"
+				v-for="number in numbers" :key="number" @mouseover="hoveredNumber = number"
+				@mouseout="hoveredNumber = null">
+				{{ number }}
+			</div>
 		</div>
 	</div>
 </template>
 
 <style>
+
+/* I made small improvements to the style in order to obtain a better user experience. */
+
+input {
+	display: block;
+	margin-block: 20px;
+}
+
+
+.container {
+	display: grid;
+	grid-template-columns: repeat(10, 1fr);
+}
+
 .number {
 	display: inline-block;
 	padding: 5px;
 	background-color: lightgrey;
 	margin: 5px;
+	text-align: center;
 }
 
 .active {
